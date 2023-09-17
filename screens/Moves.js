389010f297185fb9b2940e4485119beaf94ae13f
poxford3 <ptobmo@gmail.com>
2023-09-17 15:20:40 -0400

    fixed method toggle on move screen

--- screens/Moves.js
+++ screens/Moves.js
@@ -11,7 +11,7 @@ import {
 
 export default function Moves() {
   const [methSelect, setMethSelect] = useState("level");
-  const [selected, setSelected] = useState(true);
+  const [selected, setSelected] = useState("level");
 
   const moveItems = [
     {
@@ -19,13 +19,15 @@ export default function Moves() {
       req: 13,
       name: "Vine Whip",
       damageClass: "Physical",
+      type: "grass",
       power: 50,
       accuracy: 100,
     },
     {
       method: "level",
       req: 16,
-      name: "attack",
+      name: "attack attack attack",
+      type: "normal",
       damageClass: "Special",
       power: 50,
       accuracy: "-",
@@ -35,6 +37,7 @@ export default function Moves() {
       req: "HM01",
       name: "Cut",
       damageClass: "Physical",
+      type: "normal",
       power: 40,
       accuracy: 100,
     },
@@ -42,6 +45,7 @@ export default function Moves() {
       method: "machine",
       req: "HM02",
       name: "Flash",
+      type: "normal",
       damageClass: "Special",
       power: "-",
       accuracy: "-",
@@ -50,29 +54,40 @@ export default function Moves() {
 
   const Move = ({ item }) => {
     return (
-      <View style={styles.move}>
-        <Text>
-          {item.req} {item.name}
-        </Text>
-        <Text>{item.damageClass}</Text>
-        <Text>
-          {item.power} {item.accuracy}
-        </Text>
+      <View style={styles.moveBox}>
+        <View style={styles.move}>
+          <View style={styles.box}>
+            <Text>{item.req} </Text>
+          </View>
+          <Text numberOfLines={1} style={{ width: 100 }}>
+            {item.name}
+          </Text>
+          <Text>{item.damageClass[0]}</Text>
+          <Text>{item.type[0]}</Text>
+          <View style={styles.box}>
+            <Text>{item.power}</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>{item.accuracy}</Text>
+          </View>
+        </View>
       </View>
     );
   };
 
   const Selector = ({ method, text, selected }) => {
-    const bkgColor = selected ? "blue" : "white";
+    const bkgColor = selected == method ? "blue" : "white";
+    const textColor = selected == method ? "white" : "black";
+
     return (
       <TouchableOpacity
         style={[styles.selectorBox, { backgroundColor: bkgColor }]}
         onPress={() => {
           setMethSelect(method);
-          setSelected(!selected);
+          setSelected(method);
         }}
       >
-        <Text>{text}</Text>
+        <Text style={{ color: textColor }}>{text}</Text>
       </TouchableOpacity>
     );
   };
@@ -81,7 +96,7 @@ export default function Moves() {
     <SafeAreaView style={styles.container}>
       <View style={styles.selector}>
         <Selector method={"level"} text={"Level"} selected={selected} />
-        <Selector method={"machine"} text={"TM"} selected={!selected} />
+        <Selector method={"machine"} text={"TM"} selected={selected} />
       </View>
       <FlatList
         data={moveItems.filter((x) => x.method == methSelect)}
@@ -92,6 +107,13 @@ export default function Moves() {
 }
 
 const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "lightgrey",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    width: 50,
+  },
   container: {
     flex: 1,
     alignItems: "center",
@@ -99,9 +121,18 @@ const styles = StyleSheet.create({
   },
   move: {
     flexDirection: "row",
-    width: "80%",
+    width: "92%",
     justifyContent: "space-between",
+    alignItems: "center",
     marginVertical: 10,
+  },
+  moveBox: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  moveList: {
+    alignItems: "center",
   },
   selector: {
     paddingTop: 10,
@@ -110,7 +141,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   selectorBox: {
-    height: 30,
+    height: 40,
     width: 180,
     justifyContent: "center",
     alignItems: "center",
