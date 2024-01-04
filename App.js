bf891df5604867787812dc602659e0e9113f7a34
poxford3 <ptobmo@gmail.com>
2024-01-04 14:39:44 -0600

    changed images around

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { storeData, getData } from "./src/config/asyncStorage";
 import * as SplashScreen from "expo-splash-screen";
 import { PokemonContext } from "./src/contexts/PokemonContext";
 import { SpriteContext } from "./src/contexts/SpriteContext";
+import "expo-dev-client";
 
 // keep splash on screen while app loads
 SplashScreen.preventAutoHideAsync();
