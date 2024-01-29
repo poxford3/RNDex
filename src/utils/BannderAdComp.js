1a733d739fbfef6bf1b8e1aee1bb9706b70e6fba
poxford3 <ptobmo@gmail.com>
2024-01-29 15:24:10 -0600

    final commit

--- src/utils/BannderAdComp.js
+++ src/utils/BannderAdComp.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Platform } from "react-native";
 import {
   BannerAd,
   BannerAdSize,
   TestIds,
 } from "react-native-google-mobile-ads";
+import mobileAds from "react-native-google-mobile-ads";
+import { banner_id_android, banner_id_ios } from "../../keys";
 
-const adUnitId = __DEV__
-  ? TestIds.ADAPTIVE_BANNER
-  : "ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy";
+const id = Platform.OS == "ios" ? banner_id_ios : banner_id_android;
+const adUnitId = __DEV__ ? TestIds.ADAPTIVE_BANNER : id;
 
 export default function BannerAdComp() {
+  const [adInitialized, setAdInitialized] = useState(false);
+
+  mobileAds()
+    .initialize()
+    .then((adapterStatuses) => {
+      console.log(adapterStatuses);
+      setAdInitialized(true);
+    });
+
   return (
-    <BannerAd unitId={adUnitId} size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER} />
+    <>
+      {adInitialized ? (
+        <BannerAd
+          unitId={adUnitId}
+          size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+        />
+      ) : null}
+    </>
   );
 }
 
